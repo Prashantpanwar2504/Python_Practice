3946bd08caee065e4fa523843035c3294e254666
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-08-13 21:25:50 +0530

    1

--- 1. Basics/1_formated_string.py
+++ 1. Basics/1_formated_string.py
@@ -1,6 +1,6 @@
 print("Demo program")
 name = "Prashant Panwar" #string
-age = "25" #strinh
+age = "25" #string
 salary = 67000.561 #strinh
 
 print(f"my name is {name}, my age is {age}, my salary is {salary}")
@@ -8,7 +8,16 @@ print(type(age))
 print(type(name))
 print(type(salary))
 
-
 # this will give you error, because type() can not take more than one argument
 # print(type(name, age, salary))
 print(type(age), type(name), type(salary))
+
+
+#function to find the length of the string.
+string_test = "Hello, this is prashant panwar, how you doing."
+print(string_test.find('e'), string_test.find('g'), string_test.find('q'))
+# when you try to fins a char which  is not present
+# in the string then you will get -1 as return val
+
+
+#length of the string
