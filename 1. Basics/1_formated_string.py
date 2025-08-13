03fd71a28f3fad1cc73b1f786519e9a7cda1523f
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-08-13 21:41:13 +0530

    2

--- 1. Basics/1_formated_string.py
+++ 1. Basics/1_formated_string.py
@@ -1,7 +1,7 @@
 print("Demo program")
-name = "Prashant Panwar" #string
-age = "25" #string
-salary = 67000.561 #strinh
+name = "Prashant Panwar"  # string
+age = "25"  # string
+salary = 67000.561  # strinh
 
 print(f"my name is {name}, my age is {age}, my salary is {salary}")
 print(type(age))
@@ -12,12 +12,26 @@ print(type(salary))
 # print(type(name, age, salary))
 print(type(age), type(name), type(salary))
 
-
-#function to find the length of the string.
+# function to find the length of the string.
 string_test = "Hello, this is prashant panwar, how you doing."
 print(string_test.find('e'), string_test.find('g'), string_test.find('q'))
 # when you try to fins a char which  is not present
 # in the string then you will get -1 as return val
 
+# Replacement function for string
+course = "python for beginner"
+print(course.replace('beginner', 'absolute beginner'))
+course = course.replace('beginner', 'advance')
+print(course)
 
-#length of the string
+# In operation : to check whether string present in the string or not.
+str_cs = "hello, this is python for beginner but not for advance, so invest yourself accordingly"
+print('invests' in str_cs)
+# major difference b/w find and in operator is,
+# find function return the index of the substring,
+# but in operator return boolean value True/False
+
+
+# length of the string
+str = "hello world, this is python for beginner."
+print(len(str))
