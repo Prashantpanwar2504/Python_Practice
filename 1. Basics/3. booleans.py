01707f01397ee545346f7ecf3fe5dd0927df1e40
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-08-14 22:32:06 +0530

    boolean

--- 1. Basics/3. booleans.py
+++ 1. Basics/3. booleans.py
@@ -23,23 +23,46 @@
 # print(bool([]))  #Any list, tuple, set, and dictionary are True, except empty ones.
 
 # Some Values are False
-print(bool(False))
-print(bool(None))
-print(bool(not False))
-print(bool(not True))
-print(bool(not 0))
-print(bool(not 1))
-print(bool(""))
-print(bool(not ""))
-print(bool(()), type(())) #tuple
-print(bool({}), type({})) #dictionary
-print(bool([]), type([])) #lis
+# print(bool(False))
+# print(bool(None))
+# print(bool(not False))
+# print(bool(not True))
+# print(bool(not 0))
+# print(bool(not 1))
+# print(bool(""))
+# print(bool(not ""))
+# print(bool(()), type(())) #tuple
+# print(bool({}), type({})) #dictionary
+# print(bool([]), type([])) #list
 
 
+# if you class is returning to the object of it's class then it will be false or 0.
+class myclass():
+    def __len__(self):
+        return 0;
 
 
+my_obj = myclass()
+print(my_obj)
+print(bool(my_obj))
+print(type(my_obj))
 
+# functin can return boolean
 
+def myFunction1():
+    return True
+print(myFunction1())
 
+def myFunction2() :
+  return True
 
+if myFunction2():
+  print("YES!")
+else:
+  print("NO!")
 
+# Python also has many built-in functions that return a boolean value,
+# like the isinstance() function, which can be used to determine
+# if an object is of a certain data type:
+x = 200
+print(isinstance(x, float))
