492302eb6aff9204a892b0eaad2e207f4f0a15dd
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-08-17 23:33:24 +0530

    changes

---
+++ 1. Basics/4. Operator.py
@@ -0,0 +1,24 @@
+x = 20
+y = 3
+
+print(x + y)
+print(x * y)  # multiplication
+print(x ** y)  # power
+print(x / y)  # Division
+print(x // y)  # Floor division
+print(x % y)  # Remember
+
+
+print("-------------")
+a = 20
+# a%=3  #remenber
+# a*=3
+# a**=3
+# a//=3
+# a&=20 #bitwise AND assignment operator.
+# a|=10 #bitwise OR assignment operator.
+# a^=17 #bitwise XOR assignment operator.
+# a>>=3 #right shift assignment operator.
+# a<<=3 #left shift assignment operator.
+# print(a := 3)
+print(a)
