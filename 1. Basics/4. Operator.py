e2f375666e29a64de21b30b4ed03cc0222da9e43
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-08-18 17:11:28 +0530

    cal

--- 1. Basics/4. Operator.py
+++ 1. Basics/4. Operator.py
@@ -1,16 +1,16 @@
-x = 20
-y = 3
+# x = 20
+# y = 3
 
-print(x + y)
-print(x * y)  # multiplication
-print(x ** y)  # power
-print(x / y)  # Division
-print(x // y)  # Floor division
-print(x % y)  # Remember
+# print(x + y)
+# print(x * y)  # multiplication
+# print(x ** y)  # power
+# print(x / y)  # Division
+# print(x // y)  # Floor division
+# print(x % y)  # Remember
 
 
-print("-------------")
-a = 20
+# print("-------------")
+# a = 20
 # a%=3  #remenber
 # a*=3
 # a**=3
@@ -21,4 +21,43 @@ a = 20
 # a>>=3 #right shift assignment operator.
 # a<<=3 #left shift assignment operator.
 # print(a := 3)
-print(a)
+# print(a)
+
+
+print("=========================================")
+
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter Second number:"))
+
+print(""" Press 1 for Addition          5 for Floor Division
+                2 for Subtraction       6 for Modulus
+                3 for Multiplication    7 for Exponential
+                4 for Division""")
+
+opr = int(input("Enter your Choice:"))
+
+add = num1+num2
+sub = num1-num2
+mul = num1*num2
+div = num1/num2
+exp = (num1**num2)
+f_div = num1//num2
+mod_d = num1%num2
+
+if opr == 1:
+    print("Addition :", add)
+elif opr == 2:
+    print("Subtraction :", sub)
+elif opr == 3:
+    print("Multiplication :", mul)
+elif opr == 4:
+    print("Division :", div)
+elif opr == 5:
+    print("Floor Division :", f_div)
+elif opr == 7:
+    print("Exponential :", exp)
+else:
+    print("Modulus :", mod_d)
+
+
+
