0b8364c230d4994e5398d69757eba9fa162be686
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-08-20 22:22:19 +0530

    if_statements

---
+++ 2. Control Flow/if_elif_else_nested.py
@@ -0,0 +1,46 @@
+# # if statement
+# age = int(input("enter your age."))
+# if age >= 18:
+#     print("Valid for Voting")
+# else:
+#     print("Not a valid Candidate.")
+
+# # elif statement
+# age = int(input("Enter your age."))
+# if age < 13:
+#     print("You are a Child")
+# elif age < 18:
+#     print("You are a teenager")
+# else:
+#     print("You are an adult.")
+
+# # Nested if
+# num = float(input("Enter a Number"))
+# if num > 0:
+#     if num % 2 == 0:
+#         print("Number is Even.")
+#     else:
+#         print("Number is Odd.")
+# else:
+#     print("Number is Zero.")
+
+# Problem:check whether a given year is a leap year or not.
+
+# # logic:
+# # if year divisible by 4 but not by 100 then it is a leap year
+# # if year div by 4 and 100 then it should be div by 400 to be a leap year
+#
+# while True:
+#     year = int(input("Enter year you want to check."))
+#
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 print("Leap year")
+#             else:
+#                 print("Not a leap year")
+#         else:
+#             print("Leap year")
+#     else:
+#         print("Not a leap year")
+
