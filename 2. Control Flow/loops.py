3ce83bcce14e94769176520193d59d7d7d2ae0a9
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-08-20 22:50:14 +0530

    example

--- 2. Control Flow/loops.py
+++ 2. Control Flow/loops.py
@@ -55,3 +55,15 @@ for i in range(10):
     if i == 5:
         pass # The pass statement in Python is basically a do-nothing placeholder.
     print(i)
+
+
+
+## Example- Prime numbers between 1 and 100
+
+for num in range(1,101):
+    if num>1:
+        for i in range(2,num):
+            if num%i==0:
+                break
+        else:
+            print(num)
