9008804db9213b55e0852ce2aa71d422460460f9
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-08-21 23:47:24 +0530

    list

--- 3. Data Structure/1. List.py
+++ 3. Data Structure/1. List.py
@@ -1,1 +1,17 @@
-print("Demo")
+# 1. List
+# An ordered, mutable, and dynamic collection in Python.
+# Can hold elements of mixed data types.
+# Most commonly used data structure in Python.
+
+# Key Properties
+# Ordered → preserves insertion order.
+# Mutable → can change elements.
+# Duplicates allowed → same value can appear multiple times.
+# Indexing & slicing → supports positive & negative indexing.
+
+my_list = [1 , 2, 4, "Prashant", 2, True]
+print(my_list)
+# you can also get list element using for loop
+for i in my_list:
+    print(i)
+
