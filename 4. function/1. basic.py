1e84edb34d2a19321c6780ac5887f813bf3af2ab
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-08-27 23:50:12 +0530

    functions

---
+++ 4. function/1. basic.py
@@ -0,0 +1,68 @@
+# A function is a block of code that performs
+# a specific task. Functions help in organizing code,
+# reusing code, and improving readability.
+
+def function():
+    print("you are inside of a function.")
+
+
+function()
+print("----------------------------------")
+
+
+# Arguments:Information can be passed into functions as arguments.
+# --> Arguments are specified after the function name, inside the parentheses.
+# --> You can add as many arguments as you want, just separate them with a comma.
+
+def myname(name):
+    print("My name is " + name)
+
+
+myname("Prashant Panwar")
+
+
+def fullname(fname, lname):
+    print(fname + " " + lname)
+
+
+fullname(lname="Prashant", fname="Panwar")
+fullname("Prashant", "Panwar")  # observe the difference
+# fullname("prashant") # fullname() missing 1 required positional argument: 'lname'
+print("--------------------------------")
+
+
+# Arbitrary Arguments, *args : If you do not know how many arguments that will be
+# passed into your function, add a * before the parameter name in the function definition.
+
+def myfunction2(*args):
+    print("The youngest childrens are :" + args[3])
+
+
+myfunction2("ram", "prashant", "rahul", "aman")
+
+
+# Arbitrary Keyword Arguments, **kwargs : If you do not know how many keyword arguments that will be passed into your function,
+# add two asterisk: ** before the parameter name in the function definition.
+def myfunction3(**kwargs):
+    print("My last name is " + kwargs["lname"])
+
+myfunction3(fname="Prashant", lname="Panwar")
+print("-----------------------------------------")
+
+# default parameter
+def my_function4(country = "Norway"):
+  print("I am from " + country)
+
+my_function4("Sweden") # if you pass the parameter the it function will get it and print it.
+my_function4("India")
+my_function4() # if you don't pass the parameter then it will pass the default paramter that was asigned while you define the function
+my_function4("Brazil")
+print("------------------------------------------")
+
+# Passing a List as an Argument
+def my_function5(list):
+    for i in list:
+        print(i)
+
+numbers = [1,2,3,4,5, "d",6,7,8,9]
+my_function5(numbers)
