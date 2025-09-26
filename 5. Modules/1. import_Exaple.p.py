39877b3a2dc1be7ceafab5a5bf135ba3abceed25
Prashantpanwar2504 <prashantpanwar423361@gmail.com>
2025-09-26 12:04:33 +0530

    Error in the code:

---
+++ 5. Modules/1. import_Exaple.p.py
@@ -0,0 +1,10 @@
+from package.math import addition
+
+
+
+A = int(input("Enter A:"))
+B = int(input("Enter B:"))
+
+
+
+result = addition(A, B)
---
+++ 5. Modules/package/math.py
@@ -0,0 +1,15 @@
+def Addition(a, b):
+    return a+b
+
+def Subtraction(a, b):
+    return a-b
+
+def Multiplication(a, b):
+    return a*b
+
+def Division(a, b):
+    return a/b
+
+
+def int_Division(a, b):
+    return a//b
